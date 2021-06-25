1bc3c3dc4d055f43299e9b8dd64a16b33e6c09ca
Israel <israelliasbo@gmail.com>
2021-06-25 10:49:46 -0400

    EMPTY COMMIT

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\WeatherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
